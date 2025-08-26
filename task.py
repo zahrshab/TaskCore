3914caf836f23a7907fcacd903741a1d080789cc
zahrshab <shab_al12@hotmail.com>
2025-08-26 17:42:01 +0200

    improved: auto-incremented task-id

--- task.py
+++ task.py
@@ -1,8 +1,15 @@
 from sqlmodel import Field, SQLModel
+from typing import Optional
 
-class Task(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class TaskBase(SQLModel):
     title: str
     description: str
     priority: int
     status: str | None=Field(default = "pending")
+
+class TaskCreate(TaskBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class TaskRead(TaskBase):
+    id: int | None = Field(default=None, primary_key=True)
--- task_routes.py
+++ task_routes.py
@@ -1,50 +1,50 @@
 from fastapi import Depends, HTTPException, APIRouter
 from database import get_session
-from task import Task
+from task import TaskBase, TaskRead, TaskCreate
 from sqlmodel import Session, select
 
 router = APIRouter()
 
 @router.post("/task")
-def create_task(task: Task, session: Session = Depends(get_session)):
-    if get_task(task.id, session): 
-        raise HTTPException(status_code=406, detail="Task already exists")
+def create_task(payload: TaskBase, session: Session = Depends(get_session)):
+    task = TaskCreate(**payload.model_dump())
     session.add(task)
     session.commit()
     session.refresh(task)
-    return task
+    return {"message": f"Task is created"}
     
-@router.get("/tasks/{id}", response_model=Task)
+@router.get("/tasks/{id}", response_model=TaskRead)
 def get_task(id: int, session: Session = Depends(get_session)): 
-    found_task = session.get(Task, id)
+    found_task = session.get(TaskCreate, id)
     if not found_task:
         raise HTTPException(status_code=404, detail="Task does not exist")
     return found_task
 
-@router.get("/tasks", response_model=list[Task])
+@router.get("/tasks", response_model=list[TaskRead])
 def get_all_tasks(session: Session = Depends(get_session)): 
-    found_tasks = session.exec(select(Task)).all()
+    found_tasks = session.exec(select(TaskCreate)).all()
     if not found_tasks:
         raise HTTPException(status_code=404, detail="No available tasks")
     return found_tasks
 
 @router.patch("/tasks/{id}")
-def update_task(id: int, task: Task, session: Session = Depends(get_session)): 
-    found_task = session.get(Task, id)
+def update_task(id: int, payload: TaskBase, session: Session = Depends(get_session)): 
+    found_task = session.get(TaskCreate, id)
     if not found_task:
         raise HTTPException(status_code=406, detail="Task does not exist")
+    task = TaskCreate(**payload.model_dump())
     task_data = task.model_dump(exclude_unset=True)
     found_task.sqlmodel_update(task_data)
     session.add(found_task)
     session.commit()
     session.refresh(found_task)
-    return {"message": f"Item {id} is updated"}
+    return {"message": f"Task {id} is updated"}
 
 @router.delete("/tasks/{id}")
 def delete_task(id: int, session: Session = Depends(get_session)): 
-    found_task = session.get(Task, id)
+    found_task = session.get(TaskCreate, id)
     if not found_task:
         raise HTTPException(status_code=406, detail="Task does not exist")
     session.delete(found_task)
     session.commit()
-    return {"message": f"Item {id} deleted"}
+    return {"message": f"Task {id} deleted"}
